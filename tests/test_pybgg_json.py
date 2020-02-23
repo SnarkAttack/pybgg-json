7eb997cc57b7318778b8935af925c9fc6de1d536
Patrick McQuay <patrick.mcquay@gmail.com>
2020-02-23 08:04:18 -0500

    Cleaned up test utility functions

--- tests/test_pybgg_json.py
+++ tests/test_pybgg_json.py
@@ -12,32 +12,33 @@ expected_keys_thing_items = {
     'boardgame': ['id', 'value'],
 }
 
-def check_elem(elem):
-    print(elem)
+def check_all_element_data(elem, parent_key):
+    if parent_key is None:
+        return True
+    
+    if type(parent_key) != dict:
+        return True
+    
+    return all(item in expected_keys_thing_items[key] for item in value.keys())
+
+def check_elem(elem, parent_key):
     if type(elem) == dict:
-        for key, value in elem.items():
-            if type(value) == dict:
-                print(expected_keys_thing_items[key])
-                for x in value.keys():
-                    print(x in expected_keys_thing_items[key])
-                print(value.keys())
-                return all(item in expected_keys_thing_items[key] for item in value.keys()) and all(check_elem(item) for item in value.values())
-            elif type(value) == list:
-                return all(check_elem(item) for item in value)
-            else:
-                return True
+        check_all_element_data(elem, parent_key)
+        return all(check_elem(value, key) for key, value in elem.items())
     elif type(elem) == list:
         for item in elem:
-            return check_elem(item)
+            return check_elem(item, parent_key)
     else:
         return True
     
 def check_json(json_str):
     bgg_dict = json.loads(json_str)
-    return check_elem(bgg_dict)
+    return check_elem(bgg_dict, None)
 
 def test_thing_items_request_basic():
     bgg_int = PyBggInterface()
     assert check_json(bgg_int.thing_items_request(id=266192)) == True
+    
+
         
     
